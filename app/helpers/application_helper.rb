0ac9b9f10eb1af8e4f6ac8fea0e0d495cc14144b
Arian <arian2k@gmail.com>
2016-06-21 05:31:38 +0300

    Added Photo uploading and show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,38 @@ module ApplicationHelper
     end
   end
 
+  # Аналогично user_avatar, только возвращает миниатюрную версию
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
+  end
+
+  # Возвращает адрес рандомной фотки события, если есть, хотя бы одна
+  # Или ссылку на дефолтную картинку
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  # persisted -> in Photo Model
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
+
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
